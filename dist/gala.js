bbf58f48aca3af76706196142b19af2c44e4af52
Erik Gillespie <erik.gillespie@gmail.com>
2015-05-01 13:44:28 -0400

    Added caption that changes with images

--- dist/gala.js
+++ dist/gala.js
@@ -1,21 +1,29 @@
 var gala = (function() {
-  var LEFT_BTN = "<span class=\"fa fa-fw fa-backward gala-nav\"></span>";
-  var RIGHT_BTN = "<span class=\"fa fa-fw fa-forward gala-nav\"></span>";
+  var LEFT_BTN = '<span class="fa fa-fw fa-backward gala-nav"></span>';
+  var RIGHT_BTN = '<span class="fa fa-fw fa-forward gala-nav"></span>';
+  var FIGURE = '<figure></figure>';
+  var CAPTION = '<figcaption class="gala-caption"></figcaption>';
 
   var addButtons = function($gala, $images) {
-    var height = $gala.height() + "px";
+    var height = $gala.height() + 'px';
 
-    $images.css("left", 0); // First transition is ignored w/o this
-    $gala.before(LEFT_BTN).prev().css("line-height", height).click(getOnClickLeft($gala, $images));
-    $gala.after(RIGHT_BTN).next().css("line-height", height).click(getOnClickRight($gala, $images));
+    $images.css('left', 0); // First transition is ignored w/o this
+    $gala.before(LEFT_BTN).prev().css('line-height', height).click(getOnClickLeft($gala, $images));
+    $gala.after(RIGHT_BTN).next().css('line-height', height).click(getOnClickRight($gala, $images));
 
-    resetButtonState($gala, 0, $images.length);
+    updateButtonState($gala, 0, $images.length);
+    updateCaptionText($gala, $images, 0);
+  };
+
+  var addCaption = function($gala, $images) {
+    $gala.wrap(FIGURE).after(CAPTION);
   };
 
   var create = function(gala) {
     var $gala = $(gala);
-    var $images = $gala.children("img");
+    var $images = $gala.children('img');
 
+    addCaption($gala, $images);
     if ($images.length > 1) {
       addButtons($gala, $images);
     }
@@ -24,46 +32,54 @@ var gala = (function() {
   var getOnClickLeft = function($gala, $images) {
     return function() {
       var width = $gala.width();
-      var offset = parseInt($images.css("left"), 10);
+      var offset = parseInt($images.css('left'), 10);
       var totalImages = $images.length;
       if (offset % width != 0 || totalImages <= 1 || offset <= -(width * (totalImages-1))) {
         return;
       }
-      $images.css("left", (offset-width)+"px");
-      resetButtonState($gala, 1-offset/width, totalImages);
+      $images.css('left', (offset-width)+'px');
+      var currentImage = 1-offset/width;
+      updateButtonState($gala, currentImage, totalImages);
+      updateCaptionText($gala, $images, currentImage);
     };
   };
 
   var getOnClickRight = function($gala, $images) {
     return function() {
       var width = $gala.width();
-      var offset = parseInt($images.css("left"), 10);
+      var offset = parseInt($images.css('left'), 10);
       var totalImages = $images.length;
       if (offset % width != 0 || totalImages <= 1 || offset >= 0) {
         return;
       }
-      $images.css("left", (offset+width)+"px");
-      resetButtonState($gala, -offset/width-1, totalImages);
+      $images.css('left', (offset+width)+'px');
+      var currentImage = -offset/width-1;
+      updateButtonState($gala, currentImage, totalImages);
+      updateCaptionText($gala, $images, currentImage);
     };
   };
 
-  var resetButtonState = function($gala, currentImage, totalImages) {
+  var updateButtonState = function($gala, currentImage, totalImages) {
     if (currentImage <= 0) {
-      $gala.next().addClass("gala-disabled");
+      $gala.next().addClass('gala-disabled');
     } else {
-      $gala.next().removeClass("gala-disabled");
+      $gala.next().removeClass('gala-disabled');
     }
 
     if (currentImage >= totalImages-1) {
-      $gala.prev().addClass("gala-disabled");
+      $gala.prev().addClass('gala-disabled');
     } else {
-      $gala.prev().removeClass("gala-disabled");
+      $gala.prev().removeClass('gala-disabled');
     }
+  };
+
+  var updateCaptionText = function($gala, $images, currentImage) {
+    $gala.siblings("figcaption").text($($images[currentImage]).attr("alt"));
   };
 
   return {
     initialize: function() {
-      $(".gala").each(function(index, gala) {
+      $('.gala').each(function(index, gala) {
         create(gala);
       });
     }

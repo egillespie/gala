db84ab1b00c9d07693dd75a5985ddadc181a7c91
Erik Gillespie <erik.gillespie@gmail.com>
2015-05-01 13:02:22 -0400

    Prohibit moving to invalid slide positions

--- dist/gala.js
+++ dist/gala.js
@@ -1,28 +1,64 @@
 var gala = (function() {
-  var LEFT = "<span class=\"fa fa-fw fa-backward gala-nav\"></span>";
-  var RIGHT = "<span class=\"fa fa-fw fa-forward gala-nav\"></span>";
+  var LEFT_BTN = "<span class=\"fa fa-fw fa-backward gala-nav\"></span>";
+  var RIGHT_BTN = "<span class=\"fa fa-fw fa-forward gala-nav\"></span>";
 
-  var addButtons = function($gala) {
-    var width = $gala.width() + "px";
+  var addButtons = function($gala, $images) {
     var height = $gala.height() + "px";
 
-    var $images = $gala.children("img");
-    $images.css("left", 0);
+    $images.css("left", 0); // First transition is ignored w/o this
+    $gala.before(LEFT_BTN).prev().css("line-height", height).click(getOnClickLeft($gala, $images));
+    $gala.after(RIGHT_BTN).next().css("line-height", height).click(getOnClickRight($gala, $images));
 
-    $gala.before(LEFT).prev().click(function() {
-      $images.css("left", "+=-"+width);
-    }).css("line-height", height);
-
-    $gala.after(RIGHT).next().click(function() {
-      $images.css("left", "+="+width);
-    }).css("line-height", height);
+    resetButtonState($gala, 0, $images.length);
   };
 
   var create = function(gala) {
     var $gala = $(gala);
     var $images = $gala.children("img");
 
-    addButtons($gala);
+    if ($images.length > 1) {
+      addButtons($gala, $images);
+    }
+  };
+
+  var getOnClickLeft = function($gala, $images) {
+    return function() {
+      var width = $gala.width();
+      var offset = parseInt($images.css("left"), 10);
+      var totalImages = $images.length;
+      if (offset % width != 0 || totalImages <= 1 || offset <= -(width * (totalImages-1))) {
+        return;
+      }
+      $images.css("left", (offset-width)+"px");
+      resetButtonState($gala, 1-offset/width, totalImages);
+    };
+  };
+
+  var getOnClickRight = function($gala, $images) {
+    return function() {
+      var width = $gala.width();
+      var offset = parseInt($images.css("left"), 10);
+      var totalImages = $images.length;
+      if (offset % width != 0 || totalImages <= 1 || offset >= 0) {
+        return;
+      }
+      $images.css("left", (offset+width)+"px");
+      resetButtonState($gala, -offset/width-1, totalImages);
+    };
+  };
+
+  var resetButtonState = function($gala, currentImage, totalImages) {
+    if (currentImage <= 0) {
+      $gala.next().addClass("gala-disabled");
+    } else {
+      $gala.next().removeClass("gala-disabled");
+    }
+
+    if (currentImage >= totalImages-1) {
+      $gala.prev().addClass("gala-disabled");
+    } else {
+      $gala.prev().removeClass("gala-disabled");
+    }
   };
 
   return {

83caee89e34e4a2c48c055b4a60a01418798b63f
Erik Gillespie <erik.gillespie@gmail.com>
2015-05-01 11:05:12 -0400

    Vertically align left/right nav buttons

--- dist/gala.js
+++ dist/gala.js
@@ -1,23 +1,27 @@
 var gala = (function() {
+  var LEFT = "<span class=\"fa fa-fw fa-backward gala-nav\"></span>";
+  var RIGHT = "<span class=\"fa fa-fw fa-forward gala-nav\"></span>";
 
   var addButtons = function($gala) {
-    var width = $gala.width();
+    var width = $gala.width() + "px";
+    var height = $gala.height() + "px";
 
     var $images = $gala.children("img");
     $images.css("left", 0);
 
-    $gala.prev().click(function() {
-      $images.css("left", "+=-"+width+"px");
-    })
+    $gala.before(LEFT).prev().click(function() {
+      $images.css("left", "+=-"+width);
+    }).css("line-height", height);
 
-    $gala.next().click(function() {
-      $images.css("left", "+="+width+"px");
-    });
+    $gala.after(RIGHT).next().click(function() {
+      $images.css("left", "+="+width);
+    }).css("line-height", height);
   };
 
   var create = function(gala) {
     var $gala = $(gala);
-    var $images = $gala.find("img");
+    var $images = $gala.children("img");
+
     addButtons($gala);
   };
 
